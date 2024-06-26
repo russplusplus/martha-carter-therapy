4f1558a6a0c4aeb5df864af6ef3df393a67c4851
russplusplus <russell.kerber@gmail.com>
2024-06-26 17:36:26 -0600

    FAQ section behaves better with narrow screens

--- src/app/components/Certification.tsx
+++ src/app/components/Certification.tsx
@@ -7,34 +7,34 @@ export function Certification() {
                 <h1 id="certification-title">Education and Certifications:</h1>
                 <br/>
                 <p className="paragraph">
-                    &emsp; University of North Carolina Chapel Hill
+                    Master of Social Work: Direct Practice
                 </p>
                 <p className="paragraph">
-                    &emsp; Master of Social Work: Direct Practice
+                    Specialization: Addiction Assessment and Treatment
                 </p>
                 <p className="paragraph">
-                    &emsp; Specialization: Addiction assessment and treatment
+                    University of North Carolina Chapel Hill
                 </p>
                 <br/>
                 <p className="paragraph">
-                    &emsp; Licensed Clinical Social Worker
+                    Licensed Clinical Social Worker
                 </p>
                 <p className="paragraph">
-                    &emsp; State of Colorado
+                    State of Colorado
                 </p>
                 <p className="paragraph">
-                    &emsp; CSW.09929340
+                    CSW.09929340
                 </p>
                 <br/>
                 <p className="paragraph">
                     <a href="https://www.swtraumatraining.com/2024-post-traumatic-growth-training" target="_blank">
                         <Lavender>
-                            &emsp; Post Traumatic Growth
+                            Post Traumatic Growth
                         </Lavender>
                     </a>
                 </p>
                 <p className="paragraph">
-                    &emsp; All modules completed May 2024
+                    All modules completed May 2024
                 </p>
             </div>
         </div>
--- src/app/components/FAQ.tsx
+++ src/app/components/FAQ.tsx
@@ -1,68 +1,102 @@
+"use client"
+
+import { useState, useRef } from "react"
 import { Lavender } from "./Lavender"
 import { CallToAction } from "./CallToAction"
 
 export function FAQ() {
+
+    const [q1Breaks, setQ1Breaks] = useState(0)
+
+    const a1Ref = useRef(null)
+
     return (
         <div id='faq-row-container' className='flex-h-center'>
-            <div id="q-container" className="cream">
-                <MultiBreak n={4} />
+            <div className="faq-column">
 
-                <Question breaks={10}>
-                    What does a typical session look like?
-                </Question>
+                <div className="faq-row">
+                    <div id="q-container" className="cream">
+                        <MultiBreak n={4} />
 
-                <Question breaks={6}>
-                    What is somatic therapy?
-                </Question>
+                        <Question breaks={10}>
+                            What does a typical session look like?
+                        </Question>
+                    </div>
+                    <div id="a-container" className="cream">
+                        <MultiBreak n={4} />
 
-                <Question breaks={6}>
-                    Rates and insurance?
-                </Question>
+                        <Answer breaks={2}>
+                            Each session is devoted to your needs in that moment, which can vary from week to week. No
+                            one session looks the same, and while we ultimately want to address your larger goals, we will
+                            also stay flexible so we can tackle more urgent concerns that show up along the way. Your job is
+                            to bring your authentic self and concerns, my job is to make sure the sessions are focused and
+                            meet your short-term and long-term needs.
+                        </Answer>    
+                    </div>
+                </div>
+                <div className="faq-row">
+                    <div id="q-container" className="cream">
+                        <MultiBreak n={4} />
 
-                <Question breaks={8}>
-                    Cancellation policy?
-                </Question>
+                        <Question breaks={6}>
+                            What is somatic therapy?
+                        </Question>
+                    </div>
+                    <div id="a-container" className="cream">
+                        <MultiBreak n={4} />
 
-                <Question>
-                    Lateness?
-                </Question>
-                
-                
-            </div>
-            <div id="a-container" className="cream">
-                <MultiBreak n={4} />
+                        <Answer breaks={6}>
+                            Somatic therapy is ...
+                        </Answer>
+                    </div>
+                </div>
+                <div className="faq-row">
+                    <div id="q-container" className="cream">
 
-                <Answer breaks={2}>
-                    Each session is devoted to your needs in that moment, which can vary from week to week. No
-                    one session looks the same, and while we ultimately want to address your larger goals, we will
-                    also stay flexible so we can tackle more urgent concerns that show up along the way. Your job is
-                    to bring your authentic self and concerns, my job is to make sure the sessions are focused and
-                    meet your short-term and long-term needs.
-                </Answer>    
+                        <Question breaks={6}>
+                            Rates and insurance?
+                        </Question>
+                    </div>
+                    <div id="a-container" className="cream">
 
-                <Answer breaks={6}>
-                    Somatic therapy is ...
-                </Answer>
-
-                <Answer breaks={2}>
-                    I am currently taking United and Aetna insurance, and your copay varies depending on your
-                    specific plan. Contact your insurance to find out the exact amount. Out-of-network sessions are
-                    50 minutes, and cost $160/session.
-                </Answer>      
-
-                <Answer breaks={2}>
-                    Since I am reserving this time exclusively for you, a minimum of 72 hours notice is required for
-                    rescheduling or cancellation of an appointment. If for any reason a session is cancelled less than
-                    72 hours, you will be charged the full private pay fee. This fee is the same if you are using
-                    insurance or not, as insurance does not cover cancelled sessions.
-                </Answer>     
-
-                <Answer breaks={2}>
-                    Each session is allotted a specific time frame, therefore late arrivals will not receive an
-                    extension of time for their schedule appointments, and the original reservation fee will be
-                    charged.
-                </Answer>     
-            </div>          
+                        <Answer breaks={2}>
+                            I am currently taking United and Aetna insurance, and your copay varies depending on your
+                            specific plan. Contact your insurance to find out the exact amount. Out-of-network sessions are
+                            50 minutes, and cost $160/session.
+                        </Answer>      
+                    </div>
+                </div>
+                <div className="faq-row">
+                    <div id="q-container" className="cream">
+                        <Question breaks={8}>
+                            Cancellation policy?
+                        </Question>
+                    </div>
+                    <div id="a-container" className="cream">
+                        <Answer breaks={2}>
+                            Since I am reserving this time exclusively for you, a minimum of 72 hours notice is required for
+                            rescheduling or cancellation of an appointment. If for any reason a session is cancelled less than
+                            72 hours, you will be charged the full private pay fee. This fee is the same if you are using
+                            insurance or not, as insurance does not cover cancelled sessions.
+                        </Answer>     
+                    </div>
+                </div>
+                <div className="faq-row">
+                    <div id="q-container" className="cream">
+                        <Question>
+                            Lateness?
+                        </Question>
+                    </div>
+                    <div id="a-container" className="cream">
+                        <Answer breaks={2}>
+                            Each session is allotted a specific time frame, therefore late arrivals will not receive an
+                            extension of time for their schedule appointments, and the original reservation fee will be
+                            charged.
+                        </Answer>     
+                    </div>     
+                </div>
+               
+            </div>     
         </div>
     )
 }
