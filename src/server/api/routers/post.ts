4e6eaef61046cb80bfdac6c9d911092b76bae433
russplusplus <russell.kerber@gmail.com>
2024-07-07 19:33:15 -0600

    fixed center line alignment

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -21,14 +21,14 @@ export const postRouter = createTRPCRouter({
         service: "gmail",
         auth: {
           user: process.env.EMAIL_ADDRESS,
-          pass: process.env.EMAIL_PASSWORD
+          pass: process.env.EMAIL_APP_PASSWORD
         }
       })
 
       const mailOptions = {
         from: process.env.EMAIL_ADDRESS,
-        to: "marthacartertherapy@gmail.com",
-        // to: "russell.kerber@gmail.com",
+        // to: "marthacartertherapy@gmail.com",
+        to: "russell.kerber@gmail.com",
         subject: "New Inquiry from marthacartertherapy.com!",
         html: `
           <p>Name: ${input.firstName} ${input.lastName}</p>
