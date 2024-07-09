28e42123785f9dd543f753bca4c7a69361c75743
russplusplus <russell.kerber@gmail.com>
2024-07-08 22:34:34 -0600

    added env variables to env.js

--- src/env.js
+++ src/env.js
@@ -11,6 +11,10 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+    EMAIL_ADDRESS: z
+      .string(),
+    EMAIL_APP_PASSWORD: z
+      .string()
   },
 
   /**
@@ -29,6 +33,8 @@ export const env = createEnv({
   runtimeEnv: {
     // DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
+    EMAIL_ADDRESS: process.env.EMAIL_ADDRESS,
+    EMAIL_APP_PASSWORD: process.env.EMAIL_APP_PASSWORD
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
