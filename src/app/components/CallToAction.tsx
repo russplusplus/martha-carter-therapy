7fb7378cc8924cf91a278507e30cb6933cde7918
russplusplus <russell.kerber@gmail.com>
2024-07-12 23:48:24 -0600

    fixed book-btn-safari

--- src/app/components/CallToAction.tsx
+++ src/app/components/CallToAction.tsx
@@ -3,21 +3,22 @@
 import { useAppContext } from "./AppContext"
 import { browserName } from 'react-device-detect';
 console.log('browserName:', browserName)
+const bookBtnClass = browserName === "Safari" ? "book-btn-safari" : "book-btn"
 
 export function CallToAction(
-    { className } 
-    : 
-    { className?: string }
+  { className } 
+  : 
+  { className?: string }
 ) {
-    const { setBookingModalOpen } = useAppContext()
+  const { setBookingModalOpen } = useAppContext()
 
-    const allClassNames = "flex-h-center margin-t-l margin-b-l cream " + className
+  const allClassNames = "flex-h-center margin-t-l margin-b-l cream " + className
 
-    return (
-        <div className={allClassNames} onClick={() => setBookingModalOpen(true)}>
-          <h2 className={browserName === "Safari" ? "book-btn-safari" : "book-btn"}>
-            Book a free consultation
-          </h2>
-        </div>
-    )
+  return (
+    <div className={allClassNames} onClick={() => setBookingModalOpen(true)}>
+      <h2 className={bookBtnClass}>
+        Book a free consultation
+      </h2>
+    </div>
+  )
 }
