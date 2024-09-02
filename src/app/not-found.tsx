428c09cb4c651ea489a5d5e56ab255460aff604a
russplusplus <russell.kerber@gmail.com>
2024-09-02 12:49:41 -0600

    not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+export default function NotFound() {
+    return (
+        <>
+            <div className="flex-h-center margin-t-l margin-b-l green">
+                <h1>Not Found</h1>
+            </div>
+            <div className="flex-h-center green">
+                <p>Looks like this page doesn&apos;t exist.</p>
+            </div>
+        </>
+    )
+}
