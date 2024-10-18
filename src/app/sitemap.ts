40ee55d409d200b1cba37ac244883b2770bb6b26
russplusplus <russell.kerber@gmail.com>
2024-10-17 21:03:09 -0600

    added https:// to url in sitemap.xml

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,7 +4,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     return [
         {
             url: `https://${process.env.NEXT_PUBLIC_BASE_URL}/somatic-therapist-denver`
-        },
+        }
         // {
         //     url: `${process.env.NEXT_PUBLIC_BASE_URL}/trauma-therapy-colorado`
         // }
