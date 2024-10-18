7f0a719de5c493857b36ef5be9754143fdb328a0
russplusplus <russell.kerber@gmail.com>
2024-10-17 20:31:31 -0600

    added https:// to url in sitemap.xml

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next"
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     return [
         {
-            url: `${process.env.NEXT_PUBLIC_BASE_URL}/somatic-therapist-denver`
+            url: `https://${process.env.NEXT_PUBLIC_BASE_URL}/somatic-therapist-denver`
         },
         // {
         //     url: `${process.env.NEXT_PUBLIC_BASE_URL}/trauma-therapy-colorado`
