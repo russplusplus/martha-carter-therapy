168e40e0b7a332a71a73af5c80907c8d4c709231
russplusplus <russell.kerber@gmail.com>
2024-12-09 22:38:16 -0700

    contact dropdown working

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -7,12 +7,16 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { relative } from 'path'
 import { BrowserView, MobileView } from "react-device-detect"
+import { useState } from 'react'
 
 const log = console.log.bind(console)
 
 export function Header() {
     const pathname = usePathname()
     log('pathname:', pathname)
+
+    const [ isMenuOpen, setMenuOpen ] = useState(false)
+    
     return (
         <header className="prevent-select">
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
@@ -52,8 +56,42 @@ export function Header() {
                             className={"paragraph browser-nav-item hover-underline " + (pathname === "/faq" ? "underline" : "")}
                         >FAQ</Link>
                     </div>
+                    <div 
+                        id="browser-contact-container"
+                        onMouseEnter={() => {log("onMouseEnter")
+                            setMenuOpen(true)}}
+                        onMouseLeave={() => setMenuOpen(false)}>
+                        <Link
+                            href="https://martha-carter.clientsecure.me"
+                            className={"paragraph browser-nav-item hover-underline"}
+                            target="_blank"
+                        >Contact</Link>
+                    </div>
+                    
                     <BrowserMenu/>
                 </div>
+                {isMenuOpen &&
+                    <div 
+                        id="browser-contact-dropdown"
+                        onMouseEnter={() => setMenuOpen(true)}
+                        onMouseLeave={() => setMenuOpen(false)}
+                        >
+                        <div className="paragraph browser-contact-dropdown-item">
+                            <Link
+                                href="https://martha-carter.clientsecure.me"
+                                className="hover-underline"
+                                target="_blank"
+                            >Book a Free Consultation</Link>
+                        </div>
+                        <div className="paragraph browser-contact-dropdown-item">
+                            <Link
+                                href="https://martha-carter.clientsecure.me"
+                                className="hover-underline"
+                                target="_blank"
+                            >Reach Out</Link>
+                        </div>
+                    </div>
+                }
                 <div style={{height: '6rem'}}></div>
             </BrowserView>
             <MobileView>
